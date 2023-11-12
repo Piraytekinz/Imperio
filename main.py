20bb3761e89957e2372f5b1313a7e44bd9c02ae4
Pirayte_kinz <anangjosh8@gmail.com>
2023-11-12 15:50:07 +0000

    init

--- main.py
+++ main.py
@@ -390,6 +390,6 @@ def about_page():
     
 
 if __name__ == "__main__":
-    # from waitress import serve
-    # serve(app, host="0.0.0.0", port=8080)  
-    app.run(debug=True)
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=8080)  
+    # app.run(debug=True)
