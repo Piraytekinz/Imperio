55ff227542a89cfc50ececcfa72ad976afcc17ba
Pirayte_kinz <anangjosh8@gmail.com>
2023-11-12 15:49:05 +0000

    init

--- main.py
+++ main.py
@@ -47,20 +47,169 @@ def build():
 
 @app.route('/signin', methods=['POST'])
 def signin():
-    
-    error = None
-    email = request.form.get('email')
-    password = request.form.get('password')
-    
+    try:
+        error = None
+        email = request.form.get('email')
+        password = request.form.get('password')
+        
 
 
-    if len(email) > 0:
-        validator_check = re.search(r'^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$', email)
-                
-        if validator_check:
-            if len(password) > 6:
+        if len(email) > 0:
+            validator_check = re.search(r'^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$', email)
+                    
+            if validator_check:
+                if len(password) > 6:
+                    try:
+                        user = auth.sign_in_with_email_and_password(email, password)
+
+                        # verify_acc = os.path.exists(os.path.join(side_path, 'DentalAI', 'account.json'))
+                        
+
+                        # if not verify_acc:
+                        #     dictionary = {
+                        #         "email": "",
+                        #         "password": "",
+                        #         "localID": "",
+                        #         "idToken": ""
+                        #     }
+                        #     os.makedirs(os.path.join(side_path, 'DentalAI'))
+                            
+                        #     with open(os.path.join(side_path, 'DentalAI', 'account.json'), "a") as jsonfile:
+                        #         json.dump(dictionary, jsonfile)
+
+
+
+                        # with open(os.path.join(side_path, 'DentalAI', 'account.json'), "r") as jsonfile:
+                        #     file = json.load(jsonfile)
+
+                        # file['email'] = email
+                        # file['password'] = password
+                        # file['idToken'] = user['idToken']
+                        # file['localID'] = user['localId']
+
+                        # with open(os.path.join(side_path, 'DentalAI', 'account.json'), "w") as jsonfile:
+                        #     json.dump(file, jsonfile)
+                        
+                        return render_template('index.html')
+                        
+                    except requests.exceptions.HTTPError as e:
+                        error_json = e.args[1]
+                        error = json.loads(error_json)['error']['message']
+                        
+                        if error == 'EMAIL_NOT_FOUND':
+                            
+                            error = 'Email not found'
+                        
+                            
+                        if error == 'INVALID_PASSWORD':
+                            
+                            error = 'Invalid password'
+                            
+                        
+                        if error == 'INVALID_EMAIL':
+                            
+                            error = 'Invalid email'
+                            
+                        
+
+                        if error == "MISSING_PASSWORD":
+                            
+                            error = 'Missing password'
+
+                        if error == "INVALID_LOGIN_CREDENTIALS":
+                            
+                            error = 'Invalid login credentials'
+                    except:
+                        error = "Connection Error"
+                else:
+                    
+                    error = 'Password must be more than 6 characters'
+            else:
+                error = "Invalid email"
+        else:
+            
+            error = 'Invalid email'
+    except:
+        error = 'An error occured try again'
+
+    return render_template('login.html', error=error)
+
+
+
+
+
+@app.route('/signup', methods=['POST'])
+def signup():
+    try:
+    
+        error = None
+
+    
+        
+        
+
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        emails.clear()
+        passwords.clear()
+        emails.append(email)
+        passwords.append(password)
+
+        if len(email) > 0:
+            validator_check = re.search(r'^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$', email)
+                    
+            if validator_check:
+                if len(password) > 6:
+
+                    try:
+
+                        send_code()
+
+                        return render_template('verify.html', email=email, tries=0)
+                    except:
+                        error = "Connection error"
+
+                else:
+                    
+                    error = "Password must be more than 6 characters."
+            else:
+                error = "Invalid email"
+        else:
+            
+            error = "Invalid email"
+    except:
+        error = 'An error occured try again'
+        
+    return render_template('signup.html', error=error)
+
+
+
+
+
+@app.route('/verify', methods=["POST"])
+def verify():
+    try:
+
+        error = None
+        input_code = request.form.get("code")
+        ok = int(request.form.get("tries"))
+
+    
+        
+
+        
+        
+        
+        
+        ok += 1
+
+        
+        if ok < 6:
+            if int(input_code) == int(code):
                 try:
-                    user = auth.sign_in_with_email_and_password(email, password)
+                    
+                    user = auth.create_user_with_email_and_password(emails[0], passwords[0])
 
                     # verify_acc = os.path.exists(os.path.join(side_path, 'DentalAI', 'account.json'))
                     
@@ -73,10 +222,8 @@ def signin():
                     #         "idToken": ""
                     #     }
                     #     os.makedirs(os.path.join(side_path, 'DentalAI'))
-                        
                     #     with open(os.path.join(side_path, 'DentalAI', 'account.json'), "a") as jsonfile:
                     #         json.dump(dictionary, jsonfile)
-
 
 
                     # with open(os.path.join(side_path, 'DentalAI', 'account.json'), "r") as jsonfile:
@@ -90,178 +237,41 @@ def signin():
                     # with open(os.path.join(side_path, 'DentalAI', 'account.json'), "w") as jsonfile:
                     #     json.dump(file, jsonfile)
                     
+
                     return render_template('index.html')
-                    
+
+
                 except requests.exceptions.HTTPError as e:
                     error_json = e.args[1]
-                    error = json.loads(error_json)['error']['message']
+                    error = json.loads(error_json)['error']
                     
-                    if error == 'EMAIL_NOT_FOUND':
-                        
-                        error = 'Email not found'
+                    
+                    
                     
                         
-                    if error == 'INVALID_PASSWORD':
+                    try:
                         
-                        error = 'Invalid password'
+                        if error['message'] == 'EMAIL_EXISTS':
+                            
+                            
+                            error = "Email exists"
+                            
+                    except:
                         
-                    
-                    if error == 'INVALID_EMAIL':
                         
-                        error = 'Invalid email'
-                        
-                    
+                        error = "An unknown error occured"
 
-                    if error == "MISSING_PASSWORD":
-                        
-                        error = 'Missing password'
-
-                    if error == "INVALID_LOGIN_CREDENTIALS":
-                        
-                        error = 'Invalid login credentials'
                 except:
                     error = "Connection Error"
-            else:
-                
-                error = 'Password must be more than 6 characters'
-        else:
-            error = "Invalid email"
-    else:
-        
-        error = 'Invalid email'
-
-    return render_template('login.html', error=error)
-
-
-
-
-
-@app.route('/signup', methods=['POST'])
-def signup():
-    error = None
-
-   
-    
-    
-
-    email = request.form.get('email')
-    password = request.form.get('password')
-
-    emails.clear()
-    passwords.clear()
-    emails.append(email)
-    passwords.append(password)
-
-    if len(email) > 0:
-        validator_check = re.search(r'^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$', email)
-                
-        if validator_check:
-            if len(password) > 6:
-
-                try:
-
-                    send_code()
-
-                    return render_template('verify.html', email=email, tries=0)
-                except:
-                    error = "Connection error"
 
             else:
+                error = 'Invalid code'
                 
-                error = "Password must be more than 6 characters."
         else:
-            error = "Invalid email"
-    else:
-        
-        error = "Invalid email"
-        
-    return render_template('signup.html', error=error)
+            error = 'Too many attempts. Click resend to get a new code'
+    except:
+        error = 'An error occured try again'
 
-
-
-
-
-@app.route('/verify', methods=["POST"])
-def verify():
-    error = None
-    input_code = request.form.get("code")
-    ok = int(request.form.get("tries"))
-
-   
-    
-
-    
-    
-    
-    
-    ok += 1
-
-    
-    if ok < 6:
-        if int(input_code) == int(code):
-            try:
-                
-                user = auth.create_user_with_email_and_password(emails[0], passwords[0])
-
-                # verify_acc = os.path.exists(os.path.join(side_path, 'DentalAI', 'account.json'))
-                
-
-                # if not verify_acc:
-                #     dictionary = {
-                #         "email": "",
-                #         "password": "",
-                #         "localID": "",
-                #         "idToken": ""
-                #     }
-                #     os.makedirs(os.path.join(side_path, 'DentalAI'))
-                #     with open(os.path.join(side_path, 'DentalAI', 'account.json'), "a") as jsonfile:
-                #         json.dump(dictionary, jsonfile)
-
-
-                # with open(os.path.join(side_path, 'DentalAI', 'account.json'), "r") as jsonfile:
-                #     file = json.load(jsonfile)
-
-                # file['email'] = email
-                # file['password'] = password
-                # file['idToken'] = user['idToken']
-                # file['localID'] = user['localId']
-
-                # with open(os.path.join(side_path, 'DentalAI', 'account.json'), "w") as jsonfile:
-                #     json.dump(file, jsonfile)
-                
-
-                return render_template('index.html')
-
-
-            except requests.exceptions.HTTPError as e:
-                error_json = e.args[1]
-                error = json.loads(error_json)['error']
-                
-                
-                
-                
-                    
-                try:
-                    
-                    if error['message'] == 'EMAIL_EXISTS':
-                        
-                        
-                        error = "Email exists"
-                        
-                except:
-                    
-                    
-                    error = "An unknown error occured"
-
-            except:
-                error = "Connection Error"
-
-        else:
-            error = 'Invalid code'
-            
-    else:
-        error = 'Too many attempts. Click resend to get a new code'
-        
     return render_template('verify.html', error=error, tries=ok, email=emails[0])
 
 
